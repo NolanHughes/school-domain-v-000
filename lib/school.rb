458e009e4f3bc1f211683fa619cd618d6b492521
Nolan Hughes <nhughes987@gmail.com>
2017-11-01 18:52:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -31,7 +31,6 @@ class School
     roster.collect do |key, value|
       value.sort
     end
-    roster
   end
 # roster[key]= value.sort{ |a,b| a[1]<=>b[1] }
 # roster = {9 => ["Nolan", "Jessie"], 10 => ["Sandy", "Jonny"]}
