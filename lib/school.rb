42ef9b6955b4082478cfbcfef95bd543d70a4298
Nolan Hughes <nhughes987@gmail.com>
2017-11-01 17:59:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,11 @@ class School
   end
 
   def add_student(name, grade)    
-    roster[grade] = []
-    roster[grade] << name
+    if grade.length = nil
+      roster[grade] = []
+    else
+      roster[grade] << name
+    end
   end
 
 end
